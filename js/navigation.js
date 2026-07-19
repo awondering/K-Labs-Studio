0e38d4c7900e125f7b95e42f61fa9b6ebc031929
Kevin Boyles <kevin@klabs.co.nz>
2026-07-19 14:55:38 +1200

    Rename guide layout section to workshop

--- js/navigation.js
+++ js/navigation.js
@@ -36,7 +36,7 @@ function ensureNavMenu(){
 						<div class="component-sheet__row"><button class="component-sheet__option" type="button" data-nav="workshopScreen">Studio</button></div>
 						<div class="component-sheet__row"><button class="component-sheet__option" type="button" data-nav="buildsScreen">Saved Builds</button></div>
 						<div class="component-sheet__row"><button class="component-sheet__option" type="button" data-nav="customersScreen">Customers</button></div>
-						<div class="component-sheet__row"><button class="component-sheet__option" type="button" data-nav="layoutScreen">Guide Layout</button></div>
+						<div class="component-sheet__row"><button class="component-sheet__option" type="button" data-nav="layoutScreen">Workshop</button></div>
 						<div class="component-sheet__row"><button class="component-sheet__option" type="button" data-nav="settingsScreen">Settings</button></div>
 				</div>
 			</div>
