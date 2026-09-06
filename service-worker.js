21aafa27ca18b199d1ffb9e48865b18d8bf1c1b8
awondering <kevin@klabs.co.nz>
2026-09-07 11:51:28 +1200

    Clean up build component specifications placeholder

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_SHELL_VERSION='build096-v117';
+const APP_SHELL_VERSION='build097-v118';
 const CACHE_PREFIX='klabs-studio-app-shell-';
 const CACHE=`${CACHE_PREFIX}${APP_SHELL_VERSION}`;
 const LEGACY_CACHE_PREFIX='klabs-studio-build';
